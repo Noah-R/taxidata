5c1aa9820db6c13f609551b23ca035aa96623bd2
Noah Rutkovsky <noah.rutkovsky@gmail.com>
2021-03-20 09:58:03 -0400

    scripts both now work for a list of zones

--- counting.py
+++ counting.py
@@ -1,18 +1,18 @@
-#This script iterates through every trip all year, and for a given zone zone(See comments to modify to count up for every zone, but it'll take forever), counts up the number of hails during each quarter hour of the day. Results are exported to graphable CSV's.
+#This script iterates through every trip all year, and for a given set of zones, counts up the number of hails during each quarter hour of the day. Results are exported to graphable CSV's.
 #Further expansions of this could include narrowing results by day of week, weather, other trip metadata
 import pandas
 import numpy
 import datetime
 
-chosenzone=31
-#frames=[]
-#for i in range(265):
-data = {'TimeBucket': [], 'Amount': []}
-for time in range(96):
-    data["TimeBucket"].append(datetime.datetime(year=2000, month=1, day=1, hour=int(time/4), minute=time%4*15))
-    data["Amount"].append(0)
-frame = pandas.DataFrame.from_dict(data)
-#frames.append(frame)
+chosenzones=["31", "20", "47"]
+frames={}
+for i in range(len(chosenzones)):
+    data = {'TimeBucket': [], 'Amount': []}
+    for time in range(96):
+        data["TimeBucket"].append(datetime.datetime(year=2000, month=1, day=1, hour=int(time/4), minute=time%4*15))
+        data["Amount"].append(0)
+    frame = pandas.DataFrame.from_dict(data)
+    frames[chosenzones[i]]=frame
 
 filenames=[]
 for month in ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]:
@@ -24,19 +24,17 @@ for month in ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", 
     d=pandas.concat([green, yellow])
     green = None
     yellow = None
-    d = d[d['pickupzone'] == chosenzone]
+    d = d[d['pickupzone'].apply(lambda x: str(x)).isin(chosenzones)]
     d["datetime"] = pandas.to_datetime(d['datetime'], format='%Y-%m-%d %H:%M:%S')
     d["datetime"] = d["datetime"].apply(lambda x: x.replace(2000, 1, 1))
     for index, row in d.iterrows():
-        #zone=int(row["pickupzone"])
+        zone=str(row["pickupzone"])
         time=row["datetime"].replace(2000, 1, 1)
-        #frame=frames[zone]
+        frame=frames[zone]
         for i2, r2 in frame.iterrows():
             if(time<=r2["TimeBucket"]+datetime.timedelta(minutes=15)):
                 frame.loc[i2,"Amount"]+=1
                 break
 
-frame.to_csv("Tables/"+str(chosenzone)+" results.csv")
-
-#for num in range(len(frames)):
-#    frames[num].to_csv("Tables/"+str(num)+" results.csv")
+for zone in chosenzones:
+    frames[zone].to_csv("Tables/"+zone+" results.csv")
--- plotting.py
+++ plotting.py
@@ -1,14 +1,18 @@
+#This script takes the results from counting.py for a number of zones, and graphs them over the course of the day, normalized as a percentage of all trips in the zone for comparison purposes
+#It might make more sense to move the tabulating of percentages to the counting file, but for current purposes it works either way
 import pandas
 import numpy
 import matplotlib
 import datetime
 
-chosenzones=["20", "31"]
+chosenzones=["20", "31", "47"]
 d=pandas.read_csv("Tables/"+chosenzones[0]+" results.csv", usecols=["TimeBucket", "Amount"])
-d=d.rename(columns={'Amount': chosenzones[0]})
+d[chosenzones[0]] = d["Amount"].apply(lambda x: x/d["Amount"].sum())
+#d=d.rename(columns={'Amount': chosenzones[0]})#this replaces the above line if not normalizing for comparison purposes
 for i in range(1, len(chosenzones)):
     d2=pandas.read_csv("Tables/"+chosenzones[i]+" results.csv", usecols=["TimeBucket", "Amount"])
-    d2=d2.rename(columns={'Amount': chosenzones[i]})
+    d2[chosenzones[i]] = d2["Amount"].apply(lambda x: x/d2["Amount"].sum())
+    #d2=d2.rename(columns={'Amount': chosenzones[i]})#this replaces the above line if not normalizing for comparison purposes
     d = pandas.merge(d, d2, on="TimeBucket")
 d["TimeBucket"] = pandas.to_datetime(d['TimeBucket'], format='%Y-%m-%d %H:%M:%S')
 d.plot(x="TimeBucket", y=chosenzones)
